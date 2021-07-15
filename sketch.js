26695a907c66cc7d7f67d4f816d40cc5ae6e3d28
thakursona123 <sonathakur975@gmail.com>
2021-07-15 12:38:25 +0530

    ok

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var box6
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    box6 = new Box(200,200,100,100);
     bird = new Bird(100,100);
 
 }
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    box6.display();
     bird.display();
     platform.display();
 }
